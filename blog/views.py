ab2829783f950e9455d2dba7684f5ce9dc6ab364
amadordaniel <daniel.a@treexor.com>
2016-08-05 21:48:03 +0100

    login de usuarios de la web

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,43 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import *
 from django.shortcuts import render
-from django.utils import timezone
+from urlparse import urlparse
+
 from .models import Post
 
+
 def post_list(request):
+    current_user = request.user
+    print current_user.is_anonymous()
     posts = Post.objects.order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/post_list.html', {'posts':posts, 'current_user':current_user})
+
+
+def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+    return render(request, 'blog/login.html', {})
+
+# def logout(request, next_page=None,
+#            template_name='registration/logged_out.html',
+#            redirect_field_name='next',
+#            current_app=None, extra_context=None):
+#     """
+#     Logs out the user and displays 'You are logged out' message.
+#     """
+#     logout(request)
+#     redirect_to = request.REQUEST.get(redirect_field_name, '')
+#     if redirect_to:
+#         netloc = urlparse.urlparse(redirect_to)[1]
+#         # Security check -- don't allow redirection to a different host.
+#         if not (netloc and netloc != request.get_host()):
+#             return HttpResponseRedirect(redirect_to)
